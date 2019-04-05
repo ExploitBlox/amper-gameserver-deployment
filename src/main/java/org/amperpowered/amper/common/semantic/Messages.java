da76353747ece68b73cb99e211bce4c5336bcd55
Manuel Kollus <officialragecode@gmail.com>
2019-04-05 20:03:54 +0200

    Add the default messages

---
+++ src/main/java/org/amperpowered/amper/common/semantic/Messages.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2019, 'Amper and its contributors
+ *
+ * This code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+package org.amperpowered.amper.common.semantic;
+
+public class Messages {
+
+  private static final String PARAMETER_NULL = "Parameter \"%s\" cannot be null";
+  private static final String FIELD_NULL = "Field \"%s\" cannot be null";
+
+  private static final String PARAMETER_SIGNED_NUMBER
+      = "Parameter \"%s\" is required to be a signed %s [value=%s]";
+  private static final String PARAMETER_UNSIGNED_NUMBER
+      = "Parameter \"%s\" is required to be an unsigned %s [value=%s]";
+
+  private static final String FIELD_SIGNED_NUMBER
+      = "Field \"%s\" is required to be a signed %s [value=%s]";
+  private static final String FIELD_UNSIGNED_NUMBER
+      = "Field \"%s\" is required to be an unsigned %s [value=%s]";
+}
