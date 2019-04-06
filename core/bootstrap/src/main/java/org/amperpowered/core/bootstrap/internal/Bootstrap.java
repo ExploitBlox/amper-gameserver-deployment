7117ab8e8f3dad01e29543a87643303d2bfde82c
Manuel Kollus <officialragecode@gmail.com>
2019-04-06 11:09:20 +0200

    Add missing class header

--- core/bootstrap/src/main/java/org/amperpowered/core/bootstrap/internal/Bootstrap.java
+++ core/bootstrap/src/main/java/org/amperpowered/core/bootstrap/internal/Bootstrap.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2019, Amper and its contributors
+ *
+ * This code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 package org.amperpowered.core.bootstrap.internal;
 
 @FunctionalInterface
