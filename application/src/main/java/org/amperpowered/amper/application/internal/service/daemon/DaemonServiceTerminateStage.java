3da20d12174df9b801ead4de56fa13d582e23307
Manuel Kollus <officialragecode@gmail.com>
2019-04-15 19:44:00 +0200

    Integrate 'start' and 'terminate' stage to 'daemon' service

---
+++ application/src/main/java/org/amperpowered/amper/application/internal/service/daemon/DaemonServiceTerminateStage.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2019, Amper and its contributors
+ *
+ * This code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+package org.amperpowered.amper.application.internal.service.daemon;
+
+public class DaemonServiceTerminateStage {
+
+}
--- application/src/main/java/org/amperpowered/amper/application/internal/service/daemon/VanillaDaemonService.java
+++ application/src/main/java/org/amperpowered/amper/application/internal/service/daemon/VanillaDaemonService.java
@@ -6,16 +6,23 @@
  */
 package org.amperpowered.amper.application.internal.service.daemon;
 
+import org.amperpowered.amper.core.stage.StageFactory;
+
 public class VanillaDaemonService implements DaemonService {
 
   static final DaemonService DAEMON_SERVICE = new VanillaDaemonService();
+  private static final StageFactory STAGE_FACTORY = StageFactory.vanilla();
 
   @Override
   public void enable() {
     System.out.println("Initialized the 'daemon' service");
+
+    STAGE_FACTORY.processingStage(DaemonServiceStartStage.class);
+
   }
 
   @Override
   public void disable() {
+    STAGE_FACTORY.processingStage(DaemonServiceTerminateStage.class);
   }
 }
